46674a1dc55b3007b8b4a41678bc83e3b551afbb
mdiaz26 <mike.diaz006@gmail.com>
2020-01-21 16:51:33 -0500

    started on journal deletion logic

--- app/models/delete_journal.rb
+++ app/models/delete_journal.rb
@@ -1,2 +1,9 @@
 def delete_journal
+    #give them a list of journals
+    $prompt.select ("Which journal?") do |menu|
+        self.journal_entries do |entry|
+            menu.choice entry.title
+        end
+    end
+    # delete the journal
 end
