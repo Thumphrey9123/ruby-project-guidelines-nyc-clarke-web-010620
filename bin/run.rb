89addfbdc363f1a4ffcd918e9ec5f4b854f3a268
mdiaz26 <mike.diaz006@gmail.com>
2020-01-21 14:11:29 -0500

    added second option to menu

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,5 @@
 require_relative '../config/environment'
-prompt = TTY::Prompt.new
+$prompt = TTY::Prompt.new
 
 ##Need to add logic for entering duplicate users or locations!!
 def create_entry
@@ -13,6 +13,23 @@ def create_entry
     JournalEntry.create(user: u1, location: l1)
 end
 
+def browse_by_user
+    $prompt.select ('Choose a user:') do |menu|
+        User.all.each do |user|
+            menu.choice "#{user.name}"
+        end
+    end
+end
+
+def browse_by_rating
+end
+
+def browse_by_location
+end
+
+def delete_journal
+end
+
 # def help
 # puts "create_entry"
 # puts "other_journals"
@@ -21,12 +38,13 @@ end
 # puts "delete_journal"
 # end
 
-    prompt.select('Choose an action:') do |menu|
+    $prompt.select('Choose an action:') do |menu|
         menu.choice "create new", -> { return create_entry } 
-        menu.choice "browse by user", -> { return create_entry } 
+        menu.choice "browse by user", -> { return browse_by_user } 
         menu.choice "browse by rating", -> { return create_entry } 
         menu.choice "browse by location", -> { return create_entry } 
         menu.choice "delete a journal entry", -> { return create_entry } 
+        menu.choice "exit"
     end
 
 
