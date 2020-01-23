f63336997ab93f5b17586e7272717c4c81aa5659
mdiaz26 <mike.diaz006@gmail.com>
2020-01-23 13:29:10 -0500

    finished cleanup and notes

--- app/models/menu_methods/login.rb
+++ app/models/menu_methods/login.rb
@@ -1,14 +1,14 @@
 def login
     user_name = $prompt.ask("Please enter your Username")
     user_password = $prompt.mask("Please enter your Password")
-
+    # This logic sets a username if the credentials are correct. If not, username becomes nil and the method repeats.
     if User.all.any?{|user| user.username == user_name && user.password == user_password}
         username = User.all.find{|user| user.username == user_name}
     else
         username = nil 
     end
 
-
+# This is the repeat logic.
     if username
         puts "Welcome back #{username.username}"
         $current_user = username
