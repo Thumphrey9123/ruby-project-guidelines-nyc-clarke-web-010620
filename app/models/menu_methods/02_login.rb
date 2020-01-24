b66e6d0eaac5f1674325320f9f91515ea62bc638
github username <github email address>
2020-01-24 09:28:17 -0500

    separated login methods

--- app/models/menu_methods/02_login.rb
+++ app/models/menu_methods/02_login.rb
@@ -1,15 +1,7 @@
 def login
-    user_name = $prompt.ask("Please enter your Username")
-    user_password = $prompt.mask("Please enter your Password")
-    # This logic sets a username if the credentials are correct. If not, username becomes nil and the method repeats.
-    if User.all.any?{|user| user.username == user_name && user.password == user_password}
-        username = User.all.find{|user| user.username == user_name}
-    else
-        username = nil 
-    end
-
 # This is the repeat logic.
-    if username
+    username = get_login
+    if username 
         puts "Welcome back #{username.username}"
         $current_user = username
         start_menu
@@ -18,5 +10,15 @@ def login
         puts "Username or password do not match system records. Please try again".red
         login
     end
+end
 
+def get_login
+    user_name = $prompt.ask("Please enter your Username")
+    user_password = $prompt.mask("Please enter your Password")
+    # This logic sets a username if the credentials are correct. If not, username becomes nil and the method repeats.
+    if User.all.any?{|user| user.username == user_name && user.password == user_password}
+        username = User.all.find{|user| user.username == user_name}
+    else
+        username = nil
+    end
 end
