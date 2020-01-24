302e23687842838a9b941ad73100facf75b6a813
mdiaz26 <mike.diaz006@gmail.com>
2020-01-24 10:44:00 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,7 @@ tn = Location.create(name: "Nashville")
 sf = Location.create(name: "San Francisco")
 mx = Location.create(name: "Cancun")
 it = Location.create(name: "Rome")
+astn = Location.create(name: "Austin")
 
 def wiki_location
     Location.all.map do|location|
@@ -26,10 +27,16 @@ j1 = JournalEntry.create(user: tom, location: ny, review: "Kinda cold", title: "
 j2 = JournalEntry.create(user: tom, location: london, review: "Lots of tea", title: "Crossing the Pond", rating: 5)
 j3 = JournalEntry.create(user: tom, location: tn, review: "Got some hot chicken", title: "Gettin' country", rating: 5)
 j4 = JournalEntry.create(user: tom, location: sf, review: "TechTechTech", title: "Journey to The Valley", rating: 3)
-j5 = JournalEntry.create(user: mike, location: mx, review: "Drank too much", title: "Tequila!", rating: 5)
-j6 = JournalEntry.create(user: mike, location: it, review: "Too much gelato", title: "When in Rome.", rating: 4)
-j7 = JournalEntry.create(user: mike, location: london, review: "Lots of tea", title: "Crossing the Pond", rating: 2)
+j5 = JournalEntry.create(user: mike, location: mx, review: "Drank too much", title: "Tequila!", rating: 1)
+j6 = JournalEntry.create(user: mike, location: it, review: "The gelato was delicious", title: "When in Rome.", rating: 4)
+j7 = JournalEntry.create(user: mike, location: london, review: "London was kind of boring", title: "Crossing the Pond", rating: 2)
+j8 = JournalEntry.create(user: mike, location: london, review: "Even better the second time", title: "Return Journey", rating: 5)
+j9 = JournalEntry.create(user: tom, location: astn, review: "I love the people in this city. It's really special.", title: "Stay Weird", rating: 4)
+j10 = JournalEntry.create(user: tom, location: mx, review: "This was a perfect trip.", title: "Magic in Mexico", rating: 5)
 
-t1 = Tag.create(name: "Seafood")
-t2 = Tag.create(name: "Coastal")
-t3 = Tag.create(name: "Night Life")
+t1 = Tag.create(name: "Seafood", journal_entry: j5)
+t2 = Tag.create(name: "Coastal", journal_entry: j1)
+t3 = Tag.create(name: "Night Life", journal_entry: j1)
+t4 = Tag.create(name: "Night Life", journal_entry: j9)
+t5 = Tag.create(name: "Night Life", journal_entry: j2)
+
